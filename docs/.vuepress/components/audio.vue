c29b752a9e28c3ee8a3ffc0e43118da0f45f1301
hmlu36 <hmlu36@gmail.com>
2021-03-18 17:42:14 +0800

    'deploy'

---
+++ docs/.vuepress/components/audio.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="music-player">
+    <audio ref="audio" :src="src" preload loop id="audio" muted></audio>
+    <div @click="toggleSound()" class="toggle-sound"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+      src: "/public/sample.mp3"
+    }),
+    methods: {
+    toggleSound() {
+        let audio = this.$refs.audio;
+        if (
+          audio.paused &&
+          document.querySelector(".toggle-sound").classList.contains("paused")
+        ) {
+          console.log("play it")
+          audio.play();
+          document.querySelector(".toggle-sound").classList.remove("paused");
+        } else {
+          console.log("pause it")
+          audio.pause();
+          document.querySelector(".toggle-sound").classList.add("paused");
+        }
+      },
+    }
+};
+</script>
+
+<style scoped>
+</style>
