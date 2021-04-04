a62075873a7a775a0d1a3632dedaa016fb5a511f
joshua.lu <hmlu36@gmail.com>
2021-04-04 11:54:19 +0800

    'deploy'

---
+++ docs/.vuepress/components/backgroundMusic1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <audio-player>
+      <source src="../public/background1.mp3" />
+    </audio-player>
+  </div>
+</template>
+
+<script>
+import { audioPlayer, videoPlayer } from 'vue-md-player';
+import 'vue-md-player/dist/vue-md-player.css';
+export default {
+  components: {
+    audioPlayer,
+    videoPlayer,
+  },
+};
+</script>
---
+++ docs/.vuepress/components/ending.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <audio-player>
+      <source src="../public/Ehrling - Dance With Me.mp3" />
+    </audio-player>
+  </div>
+</template>
+
+<script>
+import { audioPlayer, videoPlayer } from 'vue-md-player';
+import 'vue-md-player/dist/vue-md-player.css';
+export default {
+  components: {
+    audioPlayer,
+    videoPlayer,
+  },
+};
+</script>
