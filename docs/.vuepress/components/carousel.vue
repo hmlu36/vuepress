20f1645b4eec45ee71bd4cc3303095ba905e1575
hmlu36 <hmlu36@gmail.com>
2021-04-12 11:33:06 +0800

    'deploy'

--- docs/.vuepress/components/carousel.vue
+++ docs/.vuepress/components/carousel.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
+    <p class="text-center content">
+      {{ images[slide].content }}
+    </p>
     <b-carousel
       v-model="slide"
+      fade
       :interval="4000"
       controls
-      indicators
       background="#ababab"
       img-width="1024"
       img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
+      style="text-shadow: 1px 1px 2px #333"
     >
       <!-- Slides with custom text -->
-      <b-carousel-slide v-for="(image, index) in images" :key="index" :img-src="image.path">
-        <h1>{{ image.content }}</h1>
-      </b-carousel-slide>
+      <template v-for="image in images">
+        <b-carousel-slide :img-src="image.path" />
+      </template>
     </b-carousel>
   </div>
 </template>
@@ -27,21 +28,16 @@ export default {
     sliding: null,
     images: [],
   }),
-  methods: {
-    onSlideStart(slide) {
-      this.sliding = true;
-    },
-    onSlideEnd(slide) {
-      this.sliding = false;
-    },
-  },
   mounted() {
-    const result = require.context('../public/photo/', true, /\.jpg$/);
+    const result = require.context("../public/photo/", true, /\.jpg$/);
 
     //console.log(JSON.stringify(result.keys()));
 
     result.keys().forEach((key) => {
-      this.images.push({ path: result(key), content: key.replace('.jpg', '').replace('.\/', '') });
+      this.images.push({
+        path: result(key),
+        content: key.replace(".jpg", "").replace(".\/", ""),
+      });
       // console.log(key);
     });
     //console.log(JSON.stringify(this.images));
@@ -49,4 +45,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.content {
+  font-size: 24px;
+}
+</style>
